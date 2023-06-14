14b0ba34eb09c2e94e940e177f38e00b46fbe699
magwoo <magwood51@yandex.ru>
2023-06-14 13:17:23 +0300

    added background

---
+++ gameplay/background/background.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+onready var texture: TextureRect = $Texture
+onready var viewport: Viewport = self.get_viewport()
+
+
+func _ready() -> void:
+	viewport.connect('size_changed', self, 'update_viewport')
+	update_viewport()
+
+
+func update_viewport() -> void:
+	texture.rect_size = viewport.size
+	texture.rect_pivot_offset = texture.rect_size / 2.0
