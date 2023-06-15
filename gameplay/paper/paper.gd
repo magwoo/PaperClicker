f84d6b633bbb0fcdba67ffdedf507d71374309f9
magwoo <magwood51@yandex.ru>
2023-06-15 23:03:10 +0300

    added paper glow

--- gameplay/paper/paper.gd
+++ gameplay/paper/paper.gd
@@ -2,14 +2,15 @@ extends Control
 
 
 export var move_range: float = 24.0
-export var move_speed: float = 1.75
+export var move_speed: float = 2.5
 export var rotation_range: float = 0.2
-export var rotation_speed: float = 1.5
-export var scale_multiply: float = 0.2
+export var rotation_speed: float = 2.25
+export var glow_rotation_speed: float = 0.5
 export(Curve) var animation_curve: Curve
 
 onready var sprite: Sprite = $Sprite
 onready var shadow: Sprite = $Sprite/Shadow
+onready var glow: Sprite = $Glow
 onready var button: Button = $Button
 onready var tween: Tween = $Tween
 
@@ -22,6 +23,8 @@ func _process(delta: float) -> void:
 	sprite.position.y = sin(Global.time * move_speed) * move_range
 	sprite.rotation = cos(Global.time * rotation_speed) * rotation_range
 	shadow.scale = Vector2.ONE - sprite.scale + Global.float2vec(1.075)
+	glow.rotate(glow_rotation_speed * delta)
+	glow.position.y = sprite.position.y
 	shadow.rotation = sprite.rotation / 3.0
 
 
