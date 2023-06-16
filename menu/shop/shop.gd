5073d0e92944059e991aaca2bca7c6e6d44e6b40
magwoo <magwood51@yandex.ru>
2023-06-16 22:19:45 +0300

    added simple shop scroll

--- menu/shop/shop.gd
+++ menu/shop/shop.gd
@@ -1,12 +1,13 @@
 class_name Shop
-extends VBoxContainer
+extends MarginContainer
 
 
-export var menu_width: float = 360.0
+export var menu_width: float = 480.0
 
 var opened: bool = false
 var tween: Tween = Tween.new()
 
+onready var container: VBoxContainer = $VBoxContainer
 onready var viewport: Viewport = self.get_viewport()
 
 
@@ -17,20 +18,22 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if _is_mouse_open() && !opened: open()
-	if !_is_mouse_open() && opened: close()
+	if _is_mouse_open(menu_width / 2.0) && !opened: open()
+	if !_is_mouse_open(menu_width * 1.5) && opened: close()
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventPanGesture: print(event.delta)
+	if event is InputEventPanGesture && opened:
+		self.margin_top -= event.delta.y * 25.0
+		var bottom_line: float = -container.rect_size.y + viewport.size.y / 1.5
+		self.margin_top = clamp(self.margin_top, bottom_line, 0.0)
 
 
 func open() -> void:
 	opened = true
 	tween.interpolate_property(
 		self, 'modulate', self.modulate, Color.white, 0.15
-	)
-	tween.interpolate_property(
+	); tween.interpolate_property(
 		self, 'rect_min_size', self.rect_min_size, Vector2(menu_width, 0.0),
 		0.2, Tween.TRANS_BACK, Tween.EASE_OUT
 	); tween.start()
@@ -40,12 +43,11 @@ func close() -> void:
 	opened = false
 	tween.interpolate_property(
 		self, 'modulate', self.modulate, Color.transparent, 0.1
-	)
-	tween.interpolate_property(
+	); tween.interpolate_property(
 		self, 'rect_min_size', self.rect_min_size, Vector2(0.0, 0.0), 0.2,
 		Tween.TRANS_BACK, Tween.EASE_OUT
 	); tween.start()
 
 
-func _is_mouse_open() -> bool:
-	return get_global_mouse_position().x > viewport.size.x - menu_width / 1.5
+func _is_mouse_open(value: float) -> bool:
+	return get_global_mouse_position().x > viewport.size.x - value
