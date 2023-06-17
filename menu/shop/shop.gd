af0d25bdc949b12c7d136d370b391e9a857be667
magwoo <magwood51@yandex.ru>
2023-06-17 15:27:26 +0300

    update shop animation and fixes

--- menu/shop/shop.gd
+++ menu/shop/shop.gd
@@ -1,13 +1,14 @@
 class_name Shop
-extends MarginContainer
+extends Control
 
 
-export var menu_width: float = 480.0
+export var menu_width: float = 256.0
 
 var opened: bool = false
 var tween: Tween = Tween.new()
 
-onready var container: VBoxContainer = $VBoxContainer
+onready var button_container: VBoxContainer = $MarginContainer/VBoxContainer
+onready var margin_container: MarginContainer = $MarginContainer
 onready var viewport: Viewport = self.get_viewport()
 
 
@@ -15,6 +16,7 @@ func _ready() -> void:
 	self.rect_min_size.x = 0.0
 	self.modulate.a = 0.0
 	self.add_child(tween)
+	close()
 
 
 func _process(delta: float) -> void:
@@ -23,10 +25,10 @@ func _process(delta: float) -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventPanGesture && opened:
-		self.margin_top -= event.delta.y * 25.0
-		var bottom_line: float = -container.rect_size.y + viewport.size.y / 1.5
-		self.margin_top = clamp(self.margin_top, bottom_line, 0.0)
+	if !event is InputEventPanGesture || !opened: return
+	self.margin_top -= event.delta.y * 25.0
+	var bottom_line: float = -button_container.rect_size.y + viewport.size.y / 1.5
+	self.margin_top = clamp(self.margin_top, bottom_line, 0.0)
 
 
 func open() -> void:
@@ -34,6 +36,9 @@ func open() -> void:
 	tween.interpolate_property(
 		self, 'modulate', self.modulate, Color.white, 0.15
 	); tween.interpolate_property(
+		margin_container, 'rect_position', margin_container.rect_position,
+		Vector2(0.0, 0.0), 0.2
+	);  tween.interpolate_property(
 		self, 'rect_min_size', self.rect_min_size, Vector2(menu_width, 0.0),
 		0.2, Tween.TRANS_BACK, Tween.EASE_OUT
 	); tween.start()
@@ -44,8 +49,11 @@ func close() -> void:
 	tween.interpolate_property(
 		self, 'modulate', self.modulate, Color.transparent, 0.1
 	); tween.interpolate_property(
-		self, 'rect_min_size', self.rect_min_size, Vector2(0.0, 0.0), 0.2,
-		Tween.TRANS_BACK, Tween.EASE_OUT
+		margin_container, 'rect_position', margin_container.rect_position,
+		Vector2(0.0, 0.0), 0.2
+	);  tween.interpolate_property(
+		self, 'rect_min_size', self.rect_min_size, Vector2(0.0, 0.0),
+		0.2, Tween.TRANS_BACK, Tween.EASE_OUT
 	); tween.start()
 
 
