f955724aaf16399b631dd1c7d4afa23b998c9403
magwoo <magwood51@yandex.ru>
2023-06-27 23:55:08 +0300

    complete load scene

--- global/load_scene/load_scene.gd
+++ global/load_scene/load_scene.gd
@@ -9,20 +9,34 @@ onready var viewport: Viewport = self.get_viewport()
 
 func _ready() -> void:
 	viewport.connect('size_changed', self, 'update_viewport')
+	tween.connect('tween_all_completed', self, 'complete')
+	logo.rect_pivot_offset = logo.rect_size / 2.0
+	logo.modulate.a = 0.0
 	self.add_child(tween)
 	animate()
 
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion && Input.is_mouse_button_pressed(1):
+		logo.rect_position += event.relative
+
+
 func animate() -> void:
-	var start_pos: Vector2 = viewport.size / 2.0
-	var end_pos: Vector2 = start_pos - Vector2(0.0, 48.0)
+	var start_pos: Vector2 = viewport.size / 2.0 - logo.rect_size / 2.0
+	var end_pos: Vector2 = start_pos - Vector2(0.0, 64.0)
 	tween.interpolate_property(
 		logo, 'rect_position', start_pos, end_pos,
-		0.3, Tween.TRANS_BACK, Tween.EASE_OUT, 0.5
+		0.6, Tween.TRANS_BACK, Tween.EASE_OUT, 0.9
 	); tween.interpolate_property(
 		logo, 'modulate', Color.transparent, Color.white,
-		0.2, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.6
+		0.4, Tween.TRANS_LINEAR, Tween.EASE_OUT, 0.9
 	); tween.start()
+
+
+func complete() -> void:
+	yield(self.get_tree().create_timer(1.5), 'timeout')
+	Music.play()
+	self.get_tree().change_scene('res://gameplay/game.tscn')
 
 
 func update_viewport() -> void:
--- global/music/music.gd
+++ global/music/music.gd
@@ -3,12 +3,24 @@ extends Node
 
 const music: AudioStream = preload('res://global/music/music.mp3')
 
+var tween: Tween = Tween.new()
+
 onready var player: AudioStreamPlayer = $AudioStreamPlayer
 
 
 func _ready() -> void:
 	player.stream = music
+	self.add_child(tween)
+
+
+func play() -> void:
+	tween.interpolate_method(self, 'set_volume', -30.0, -10.0, 2.0)
+	tween.start()
 	player.play()
+
+
+func set_volume(value: float) -> void:
+	AudioServer.set_bus_volume_db(1, value)
 
 
 func set_player_status(status: bool) -> void:
