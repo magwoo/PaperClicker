e44ac19ee80df902e2cdf9c3fd2724705a34254e
magwoo <magwood51@yandex.ru>
2023-06-27 22:57:20 +0300

    added sounds mute

--- global/classes/settings.gd
+++ global/classes/settings.gd
@@ -2,10 +2,15 @@ class_name Settings
 extends Reference
 
 
-var sounds: bool = true
+var sounds: bool = true setget update_sounds
 var music: bool = true setget update_music
 
 
 func update_music(value: bool) -> void:
 	Music.set_player_status(value)
 	music = value
+
+
+func update_sounds(value: bool) -> void:
+	AudioServer.set_bus_mute(2, value)
+	sounds = value
